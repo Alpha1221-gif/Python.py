e529dd9cfacc89e6ced415e42a309d4ed2bccd97
Alpha1221-gif <ishan21780@gmail.com>
2026-07-15 13:19:50 +0530

    lists

--- list and arrays/Introduction.py
+++ list and arrays/Introduction.py
@@ -1,1 +1,8 @@
-print("hello world")
+'''In Python, lists are the built-in data structure that serves as a dynamic array.
+Lists are ordered, mutable, and can contain elements of different types.'''
+
+#List are denoted by square brackets[]
+                                        # Example #    
+x = [1,2,89,43,100,345]
+y = ["a","b","apple","mango"]
+z = [23,45,"iron man","tiger",True]
