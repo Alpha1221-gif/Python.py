0169865e0c6fb795248802a49db98a94b6c2d40c
Alpha1221-gif <ishan21780@gmail.com>
2026-07-15 20:45:41 +0530

    commit changes

--- list and arrays/list_algorithm.py
+++ list and arrays/list_algorithm.py
@@ -8,9 +8,9 @@ print('Lowest value:', minval)   #Lowest value: 10
 
 #Create an algorithm to find the highest value in a list
 num = [45,67,23,89,10,57,98]
-minval = num[0]
+maxval = num[0]
 for i in num:
-    if (i<minval):
-        i = minval
-print('highest value:', i) #highest value: 98
+    if (i>maxval):
+        maxval = i
+print('highest value:', maxval) #highest value: 98
 
