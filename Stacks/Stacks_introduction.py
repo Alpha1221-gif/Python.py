3c60668e4b9693ab4ca9f58d6c9cf9909d68565f
Alpha1221-gif <ishan21780@gmail.com>
2026-07-15 21:02:54 +0530

    intoduction of stacks

--- Stacks/Stacks_introduction.py
+++ Stacks/Stacks_introduction.py
@@ -1,1 +1,19 @@
+'''A stack is a linear data structure that follows the Last-In-First-Out (LIFO) principle
+Think of it like a stack of pancakes - you can only add or remove pancakes from the top'''
 
+'''Stacks
+A stack is a data structure that can hold many elements, and the last element added is the first one to be removed.
+
+Like a pile of pancakes, the pancakes are both added and removed from the top. So when removing a pancake, it will always be the last pancake you added. This way of organizing elements is called LIFO: Last In First Out.
+
+Basic operations we can do on a stack are:
+
+-> Push: Adds a new element on the stack.
+-> Pop: Removes and returns the top element from the stack.
+-> Peek: Returns the top (last) element on the stack.
+-> isEmpty: Checks if the stack is empty.
+-> Size: Finds the number of elements in the stack.
+
+Stacks can be implemented by using arrays or linked lists.
+Stacks can be used to implement undo mechanisms, to revert to previous states, to create algorithms for depth-first search in graphs, or for backtracking.
+Stacks are often mentioned together with Queues, which is a similar data structure described on the next page.'''
